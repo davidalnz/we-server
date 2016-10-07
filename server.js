f2497efc4fc2e218e34726fc8a5fe4a5d4c95507
Dee Alnz <davidalnz@gmail.com>
2016-10-07 10:07:42 +0800

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require("express");
+var app = express();
+var PORT = 3000;
+var middleware = require("./middleware");
+
+
+
+app.use(middleware.logger);
+
+app.get("/about", middleware.requireAunthentication, function(req, res){
+	res.send("about us");
+});
+
+app.use(express.static(__dirname + "/public"));
+
+
+app.listen(PORT, function(){
+	console.log("Express server started");
+	console.log("Port Number: " + PORT);
+});
